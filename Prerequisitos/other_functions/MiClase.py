b94c4f94d9a3b5d7a664d1338ceddb27eff2c9ca
AJFlecher <40211802+AJFlecher@users.noreply.github.com>
2020-10-07 00:39:47 -0500

    Add files via upload

---
+++ Prerequisitos/other_functions/MiClase.py
@@ -0,0 +1,13 @@
+class MiClase:
+    def __init__(self, nombre="pow2", dato=1:
+        self.nombre=nombre
+        self.dato=dato
+        
+    def pow2(self, dato):
+        self.dato=dato
+            
+    def mod2(self, dato):
+        self.dato=dato
+                 
+    def pow3(self, dato):
+        self.dato=dato
